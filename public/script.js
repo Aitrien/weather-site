90b2a23fc68357b4f3513736a12beeece6e1e150
Aitrien <aitriens@gmail.com>
2022-05-02 22:25:22 +1200

    Measurement toggle implemented

--- public/script.js
+++ public/script.js
@@ -1,19 +1,41 @@
+// main temperature display
 const tempDisplay = document.getElementById('real-temp');
-const tempKelvin = tempDisplay.textContent.split("°")[0];
+// table elements
+const feelsDisplay = document.getElementById('display-feels');
+const highLowDisplay = document.getElementById('display-highlow');
+const windDisplay = document.getElementById('display-wind');
+const visibDisplay = document.getElementById('display-visibility');
+
+const rawData = {
+	"rawTemp" : tempDisplay.textContent,
+	"rawFeels" : feelsDisplay.textContent,
+	"rawHighLow" : highLowDisplay.textContent.split("/"),
+	"rawWind" : windDisplay.textContent,
+	"rawVisib" : visibDisplay.textContent
+}
+
+const fahrenheitBtn = document.getElementById('fahrenheit-btn');
+const celsiusBtn = document.getElementById('celsius-btn');
 let tempFactor = 1;
 let tempOffset = 273.15;
 let degreeSymbol = "C"
 
-function updateTemp() {
-	tempDisplay.textContent = `${ Math.round(tempKelvin * tempFactor - tempOffset) }°${ degreeSymbol }`;
-}
+loadPreference();
 
+function updateTemp() {
+	tempDisplay.textContent = `${ Math.round(rawData["rawTemp"] * tempFactor - tempOffset) }°${ degreeSymbol }`;
+}
 updateTemp();
 
 function swapCelsius() {
 	tempFactor = 1;
 	tempOffset = 273.15;
 	degreeSymbol = "C";
+	feelsDisplay.textContent = `${ Math.round(rawData["rawFeels"] * tempFactor - tempOffset) }°${ degreeSymbol }`;
+	highLowDisplay.textContent = `${ Math.round(rawData["rawHighLow"][0] * tempFactor - tempOffset) }°/${ Math.round(rawData["rawHighLow"][1] * tempFactor - tempOffset) }°${ degreeSymbol }`;
+	windDisplay.textContent = `${ rawData["rawWind"] } km/h`;
+	visibDisplay.textContent = `${ (rawData["rawVisib"] / 1000).toPrecision(3) } km`;
+	localStorage.setItem("tempPref", "celsius");
 	updateTemp();
 }
 
@@ -21,9 +43,34 @@ function swapFahrenheit() {
 	tempFactor = 1.8;
 	tempOffset = 459.67;
 	degreeSymbol = "F";
+	feelsDisplay.textContent = `${ Math.round(rawData["rawFeels"] * tempFactor - tempOffset) }°${ degreeSymbol }`;
+	highLowDisplay.textContent = `${ Math.round(rawData["rawHighLow"][0] * tempFactor - tempOffset) }°/${ Math.round(rawData["rawHighLow"][1] * tempFactor - tempOffset) }°${ degreeSymbol }`;
+	windDisplay.textContent = `${ (rawData["rawWind"] * 0.6213712).toPrecision(3) } mph`;
+	visibDisplay.textContent = `${ (rawData["rawVisib"] * 0.6213712 / 1000).toPrecision(3) } miles`;
+	localStorage.setItem("tempPref", "fahrenheit");
 	updateTemp();
 }
 
+// Load the user's preferred measurements
+function loadPreference() {
+	userPreference = localStorage.getItem("tempPref");
+	switch(userPreference) {
+		case "celsius":
+			celsiusBtn.checked = true;
+			swapCelsius();
+			break;
+		case "fahrenheit":
+			fahrenheitBtn.checked = true;
+			swapFahrenheit();
+			break;
+		case null:
+			celsiusBtn.checked = true;
+			localStorage.setItem("tempPref", "celsius");
+			swapCelsius();
+	}
+}
+
+// Handles the search bar functionality and submits valid inputs
 function handleForm(e) {
 	const form = document.getElementById('search');
 	const searchBar = document.getElementById('location');
--- routes/weather.js
+++ routes/weather.js
@@ -10,17 +10,36 @@ router.use(express.static('public'));
 
 // As weather is currently the only option, redirect to home page
 router.get('/', (req, res, next) => {
-	//res.send("default weather page");
 	res.redirect('/');
 	next();
 });
 
+// When the url is a valid location (code 200), display weather page
 router.get('/:location', (req, res) => {
 	getWeather(req.params.location)
 	.then(response => {
 		if (response.cod === 200) {
 			// Location searched exists, render forecast html
-			res.render('forecast', response);
+			//res.render('forecast', response);
+			res.render('forecast', 
+				{	
+					location : response.name,
+					country : response.sys.country,
+					time : new Date(response.dt * 1000).toLocaleString(),
+					temp : response.main.temp,
+					weather : response.weather[0].main,
+					description : response.weather[0].description,
+					temp_feels : response.main.feels_like,
+					temp_high : response.main.temp_max,
+					temp_low : response.main.temp_min,
+					pressure : response.main.pressure,
+					humidity : response.main.humidity,
+					wind_speed : response.wind.speed,
+					wind_dir : response.wind.deg,
+					visibility : response.visibility,
+					time_sunrise : new Date(response.sys.sunrise * 1000).toLocaleTimeString(),
+					time_sunset : new Date(response.sys.sunset * 1000).toLocaleTimeString()
+				});
 		}	else {
 			// Location searched does not exist, render unknown html (to be implemented)
 			res.send(`Unknown location: ${ req.params.location }`);
